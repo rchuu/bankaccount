5d1bd1b7eb728c95a294260947221965d900e03c
Richard Chu <rchu1211@gmail.com>
2022-07-21 22:02:58 -0700

    changes

--- BankAccountClass.java
+++ BankAccountClass.java
@@ -1,3 +1,110 @@
-public class BankAccount {
+import java.util.Random;
+import java.util.ArrayList; // 
+import java.util.Collections; // 
+import java.lang.reflect.Array; // for array creation
 
+public class BankAccount { // Create a BankAccount class.
+    private String accountNumber;
+    private Double checkingBalance;
+    private Double savingsBalance;
+
+    private static int numberOfAccounts = 0;
+    private static double totalBalance = 0.0;
+
+    public BankAccount(String newNumber, Double newChecking, Double Savings) {
+        accountNumber = random10();
+        checkingBalance = newChecking;
+        savingsBalance = Savings;
+        setNumberOfAccounts(getNumberOfAccounts() + 1);
+    }
+
+    // getter
+    public static int getPeopleCount() {
+        return getNumberOfAccounts();
+    }
+
+    // setter
+    public void setNumberOfAccounts(int number) {
+        setNumberOfAccounts(number);
+    }
+
+    // getter
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    // setter
+    public void setCheckingBalance(double checkingBalance) {
+        checkingBalance = checkingBalance + checkingBalance;
+    }
+
+    // getter
+    public double getSavingsBalance() {
+        return savingsBalance;
+    }
+
+    // setter
+    public void setSavingsBalance(double savingsBalance) {
+        savingsBalance = savingsBalance + savingsBalance;
+    }
+
+    // getter
+    public static int getNumberOfAccounts() {
+        return numberOfAccounts;
+    }
+
+    public static void setNumberofAccouts(int numberOfAccounts) {
+        BankAccount.numberOfAccounts = numberOfAccounts;
+    }
+
+    // Random Account Number Generator
+    private String random10() {
+        Random ran = new Random();
+        String charList = "0123456789";
+        StringBuffer accountNum = new StringBuffer();
+        for (int i = 0; i < 10; i++) {
+            accountNum.append(charList.charAt(ran.nextInt(charList.length())));
+        }
+    }
+
+    // Make a depost
+    public void deposit(String accountType, double value) {
+        if (accountType == "Checking") {
+            checkingBalance = checkingBalance + value;
+            totalBalance = totalBalance + value;
+        } else { System.out.println("Account Type Error, deposit of " + value + " has failed");
+    }
+    if (accountType == "Savings") {
+        savingsBalance = savingsBalance + value;
+        totalBalance = totalBalance + value;
+    } else { System.out.println("Account Type Error, deposit of " + value + " has failed")
+}}
+
+    // Make a withdrawal
+    public double withdrawl(String type, double value) {
+
+        if (type == "Checking") {
+            if (value > checkingBalance) {
+                System.out.println("Insufficient Funds");
+                return checkingBalance;
+            } else {
+                checkingBalance = checkingBalance - value;
+                totalBalance = totalBalance - value;
+                return value;
+            }
+        }
+
+        if (type == "Savings") {
+            if (value > savingsBalance) {
+                System.out.println("Insufficient Funds");
+                return savingsBalance;
+            }
+            savingsBalance = savingsBalance - value;
+            totalBalance = totalBalance - value;
+            return value;
+        } else {
+            System.out.println("Account Type Error, could not complete withdrawl of " + value + " not completed");
+            return value;
+        }
+    }
 }
---
+++ BankAccountTest.java
@@ -0,0 +1,6 @@
+public class BankAccountTest {
+    public static void main(String[] args) {
+        BankAccount user1 = new BankAccount("", 0.0, 0.0);
+        System.out.println(user1.getNumberOfAccounts());
+    }
+}
